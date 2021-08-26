f4256495d13f88666df90932868024d665098200
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-08-25 23:30:33 -0400

    Initial commit

---
+++ Assets/Scripts/FPSNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class FPSNetworkManager : NetworkManager
+{
+    public GameObject playerSpawn;
+
+    [AddComponentMenu("")]
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        Debug.Log("Called OnServerAddPlayer");
+        Debug.Log(numPlayers);
+        // add player at correct spawn position
+        Transform start = playerSpawn.transform;
+        GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        // call base functionality (actually destroys the player)
+        base.OnServerDisconnect(conn);
+    }
+}
---
+++ Assets/Scripts/FPSNetworkPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class FPSNetworkPlayer : NetworkBehaviour
+{
+    public float speed;
+
+    [AddComponentMenu("")]
+    void FixedUpdate()
+    {
+        if (hasAuthority)
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime);
+    }
+
+    public override void OnStartAuthority() {
+        Camera camera = transform.Find("FirstPersonCharacter").GetComponent<Camera>();
+        camera.enabled = true;
+        
+    }
+}
