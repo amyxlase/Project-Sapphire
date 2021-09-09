753d86c99cc87df177d754c27b2268574d442755
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-09-09 09:24:43 -0400

    better animation used, replicates

--- Assets/Scripts/FPSNetworkPlayer.cs
+++ Assets/Scripts/FPSNetworkPlayer.cs
@@ -18,7 +18,7 @@ public class FPSNetworkPlayer : NetworkBehaviour
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.E)) {
             networkAnimator.ResetTrigger("Shoot");
             networkAnimator.SetTrigger("Shoot");
             Debug.LogFormat("Should have played animation");
