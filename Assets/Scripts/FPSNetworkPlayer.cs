e06061555367d02077b306de6945c6bc2e0cdac9
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-09-09 17:05:54 -0400

    Revert "Revert "i hate everything""
    This reverts commit 50d8abd1c35a61bb42c69ce3a10483e7222150eb.

--- Assets/Scripts/FPSNetworkPlayer.cs
+++ Assets/Scripts/FPSNetworkPlayer.cs
@@ -18,10 +18,46 @@ public class FPSNetworkPlayer : NetworkBehaviour
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
+        /*if (Input.GetKeyDown(KeyCode.E)) {
             networkAnimator.ResetTrigger("Shoot");
             networkAnimator.SetTrigger("Shoot");
-            Debug.LogFormat("Should have played animation");
+            Debug.Log("Shots fired");
+
+            RaycastHit hit;
+            Ray fromCamera = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(fromCamera, out hit)) {
+                Debug.LogFormat("Raycast hit collider: {0}", hit.transform.gameObject.name);
+                Debug.DrawRay(fromCamera.origin, fromCamera.direction, Color.red, 20, true);
+
+
+                Transform target = hit.transform;
+                SkinnedMeshRenderer smr = target.GetChild(1).GetComponent<SkinnedMeshRenderer>();
+
+                if (smr != null) {
+                    Debug.LogFormat("SKinned mesh renderer not null");
+                    Material[] mats = smr.materials;
+                    mats[0] = (Material)Resources.Load("Prefabs/YellowSmooth");
+                    smr.materials = mats;
+                }
+            } else {
+                Debug.Log("Nothing was shot");
+            }
+        }*/
+
+        //shot debug
+        if (Input.GetKeyDown(KeyCode.F)) {
+            networkAnimator.ResetTrigger("Shoot");
+            networkAnimator.SetTrigger("Shoot");
+            Debug.Log("Shots fired");
+
+            RaycastHit hit;
+            Ray fromCamera = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(fromCamera.origin, fromCamera.direction, out hit, Mathf.Infinity)) {
+                Debug.LogFormat("hit registered {0} of origin {1} and direction/length {2}", hit.transform.gameObject.name, fromCamera.origin, fromCamera.direction);
+                Debug.DrawRay(fromCamera.origin, fromCamera.direction*1000000, Color.red, 1000, true);
+            } else {
+                Debug.Log("Nothing was shot");
+            }
         }
     }
 
