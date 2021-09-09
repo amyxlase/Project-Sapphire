2f8b4f9f2df8d14909c350e8b262819a19c32f98
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-09-09 08:52:11 -0400

    walking transition bug fixed

--- Assets/Scripts/FPSNetworkPlayer.cs
+++ Assets/Scripts/FPSNetworkPlayer.cs
@@ -6,12 +6,22 @@ using Mirror;
 public class FPSNetworkPlayer : NetworkBehaviour
 {
     public float speed;
+    public Animator anim;
 
     [AddComponentMenu("")]
     void FixedUpdate()
     {
+        anim.SetFloat("Vertical", Mathf.Abs(Input.GetAxis("Vertical")));
         if (hasAuthority)
             transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime);
+    }
+
+    void Update() {
+        Debug.LogFormat("Should have played animation: {0}", Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            anim.Play("Base Layer.Shoot", 0, 0.25f);
+        }
     }
 
     public override void OnStartAuthority() {
