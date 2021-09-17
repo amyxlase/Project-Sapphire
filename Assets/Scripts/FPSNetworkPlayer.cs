e8402750cfd448ef8db3166c0ef01ea3913e8626
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-09-17 17:47:19 -0400

    proper debug

--- Assets/Scripts/FPSNetworkPlayer.cs
+++ Assets/Scripts/FPSNetworkPlayer.cs
@@ -8,6 +8,7 @@ public class FPSNetworkPlayer : NetworkBehaviour
     public float speed;
     public Animator anim;
     public NetworkAnimator networkAnimator;
+    public Collider collider;
 
     [AddComponentMenu("")]
     [ClientCallback]
@@ -18,21 +19,32 @@ public class FPSNetworkPlayer : NetworkBehaviour
             transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime);
     }
 
+
     void Update() {
         
         if (Input.GetKeyDown(KeyCode.F)) {
             networkAnimator.ResetTrigger("Shoot");
             networkAnimator.SetTrigger("Shoot");
-
             Debug.Log("Shots fired");
 
-            RaycastHit hit;
-            Ray fromCamera = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(fromCamera.origin, fromCamera.direction, out hit, Mathf.Infinity)) {
-                Debug.LogFormat("hit registered {0} of origin {1} and direction/length {2}", hit.transform.gameObject.name, fromCamera.origin, fromCamera.direction);
-                Debug.DrawRay(fromCamera.origin, fromCamera.direction*1000000, Color.red, 1000, true);
-            } else {
-                Debug.Log("Nothing was shot");
+            CapsuleCollider[] colliders = FindObjectsOfType<CapsuleCollider>();
+            foreach (Collider collider in colliders) {
+                Debug.DrawRay(collider.transform.position, Vector3.up*1000000, Color.green, 1000, true);
+
+                RaycastHit hit;
+                Ray fromCamera = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (collider.Raycast(fromCamera, out hit, Mathf.Infinity)) {
+                    Debug.LogFormat("hit registered {0} of origin {1} and direction/length {2}", hit.transform.gameObject.name, fromCamera.origin, fromCamera.direction);
+
+                    if (hit.transform.gameObject.name == "FPSNetworkPlayerController(Clone)") {
+                        Debug.DrawRay(fromCamera.origin, fromCamera.direction*1000000, Color.blue, 1000, true);
+                    } else {
+                        Debug.DrawRay(fromCamera.origin, fromCamera.direction*1000000, Color.red, 1000, true);
+                    }
+
+                } else {
+                    Debug.Log("Nothing was shot");
+                }
             }
         } 
     }
