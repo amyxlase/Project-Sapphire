bb1a6a53e513b0e3cafaaa67dd654f6c652222ea
Adrian Jenkins <adrian.jenkins99@gmail.com>
2021-10-27 19:37:53 -0400

    Added raining bots

--- Assets/Scripts/FPSNetworkManager.cs
+++ Assets/Scripts/FPSNetworkManager.cs
@@ -8,6 +8,9 @@ public class FPSNetworkManager : NetworkManager
     public GameObject botSpawn;
     public GameObject botPrefab;
 
+    public int botCount = 0;
+    public int playerCount = 0;
+
     [AddComponentMenu("")]
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
@@ -15,7 +18,8 @@ public class FPSNetworkManager : NetworkManager
         start.position += Vector3.right * numPlayers;
         GameObject player = Instantiate(playerPrefab, start.position, start.rotation);
         NetworkServer.AddPlayerForConnection(conn, player);
-        GameObject bot = Instantiate(botPrefab, start.position, start.rotation);
+        playerCount++;
+        
 
         //Activate various things
         FPSNetworkPlayer playerScript = player.GetComponent<FPSNetworkPlayer>();
@@ -28,6 +32,29 @@ public class FPSNetworkManager : NetworkManager
     {
         // call base functionality (actually destroys the player)
         base.OnServerDisconnect(conn);
+    }
+
+
+    public void addBot() {
+        Transform botStart = botSpawn.transform;
+        Vector3 positionOffset = new Vector3(Random.Range(1, 20), Random.Range(1, 20), Random.Range(1, 20));
+        GameObject bot = Instantiate(botPrefab, botStart.position + positionOffset, botStart.rotation);
+        botCount++;
+        Debug.Log("Bot Count: " + botCount);
+    }
+
+    private void Update() {
+        if(botCount < playerCount * 5) {
+            addBot();
+        }
+    }
+
+    public override void OnStartServer() {
+        Debug.Log(playerCount);
+        for(int i = 0; i < 10; i++) {
+           addBot();
+        }
+        
     }
 
 }
