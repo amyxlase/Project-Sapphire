4aa20204f40a65b344f79eb9117b127837b8e0bd
amyxlase <31298710+amyxlase@users.noreply.github.com>
2021-10-27 22:29:13 -0400

    misc cleanup + temporary bot movement for beta (bot health not replicating yet)

--- Assets/Scripts/FPSNetworkBot.cs
+++ Assets/Scripts/FPSNetworkBot.cs
@@ -6,31 +6,48 @@ using Mirror;
 public class FPSNetworkBot : NetworkBehaviour
 {
     private CharacterController m_BotController;
+    private Vector3 direction;
 
     void Start() {
         m_BotController = GetComponent<CharacterController>();
+        direction = Vector3.zero;
     }
 
-    void Update()
-    {
-        //AI code should go here, these steps are a rough guideline but totally optional
+    private void FixedUpdate() {
+        
+        ApplyGravity();
+        AI();
 
+        //Temporary for beta
+        BadAI();
+    }
+
+    private void ApplyGravity() {
+        float multiplier = 55;
+        m_BotController.Move(Physics.gravity * multiplier * Time.deltaTime * Time.deltaTime);
+    }
+
+    //Walk in a random direction & change that direction every 100 frames
+    private void BadAI() {
+
+        //Randomly generate direction every 100 frames
+        if (Time.frameCount % 100 == 0) {
+            float x = Random.Range(-1f, 1f);
+            float z = Random.Range(-1f, 1f);
+            direction = new Vector3(x, 0f, z);
+        }
+
+        //Move with speed multiplier
+        float multiplier = 1f;
+        m_BotController.Move(multiplier * direction * Time.deltaTime);
+    }
+
+    //AI code should go here, these steps are a rough guideline but totally optional
+    private void AI() {
         //1. Collect information on where other players are, health, etc
 
         //2. Logic to decide action (walk in direction of players, avoid obstacles, shoot, etc)
 
-        //3. Execute choen action
-    }
-
-    private void FixedUpdate() {
-        float speed = 55;
-        Vector3 vel = transform.up;
-        Vector3 gravity = Physics.gravity;
-        Vector3 vSpeed = Vector3.zero;
-        
-        vSpeed += gravity * speed * Time.deltaTime;
-        vel = vSpeed;
-        m_BotController.Move(vel * Time.deltaTime);
-        
+        //3. Execute chosen action
     }
 }
