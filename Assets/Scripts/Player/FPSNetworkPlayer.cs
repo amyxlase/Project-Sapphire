720cd3e5d420ada2b065bc35a2769e9dc3becfbf
whuang1202 <whuang3@andrew.cmu.edu>
2021-10-31 17:32:37 -0400

    kinda works :D
    yee

--- Assets/Scripts/Player/FPSNetworkPlayer.cs
+++ Assets/Scripts/Player/FPSNetworkPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 
 public class FPSNetworkPlayer : NetworkBehaviour
@@ -14,10 +15,24 @@ public class FPSNetworkPlayer : NetworkBehaviour
     public GameObject crosshair2;
     public GameObject Dead;
     public GameObject leaderboard;
+    public GameObject healthUI;
+    public Slider HP;
 
     public bool isActive;
 
     [AddComponentMenu("")]
+
+    public void configureHP() {
+        // Configure health bar
+        healthUI = transform.GetChild(4).gameObject;
+        healthUI.SetActive(true);
+        HP = healthUI.transform.GetChild(0).gameObject.GetComponent<Slider>();
+        HP.maxValue = 100f;
+        HP.value = 100f;
+        Debug.Log(HP);
+        Debug.Log(HP.maxValue);
+        Debug.Log(HP.value);
+    }
 
     public override void OnStartAuthority() {
 
@@ -37,11 +52,17 @@ public class FPSNetworkPlayer : NetworkBehaviour
         Transform fpc = transform.Find("FirstPersonCharacter");
         fpc.GetComponent<Camera>().enabled = true;
         fpc.GetComponent<AudioListener>().enabled = true;
+
+        configureHP();
         isActive = true;
     }
 
     void Update() {
         if (!hasAuthority) return;
+
+        if (HP == null) {
+            Debug.Log("HP is null");
+        }
 
         ScopeToggle();              // Bound to Q
         Shoot();                    // Bound to F
@@ -103,6 +124,13 @@ public class FPSNetworkPlayer : NetworkBehaviour
         }
     }
 
+    public void setHP(float newValue) {
+        if (HP == null) {
+            Debug.Log("HP is null");
+        }
+        HP.value = newValue;
+    }
+
     [Command]
     public void CmdDealDamage(Transform target) {
         Debug.Log("Asked server to deal damage");
@@ -110,7 +138,14 @@ public class FPSNetworkPlayer : NetworkBehaviour
         if (target.gameObject.name == "FPSNetworkPlayerController(Clone)") {
             Debug.Log("player target");
             Damageable playerDamage = target.gameObject.GetComponent<Damageable>();
+            Health playerHealth = target.gameObject.GetComponent<Health>();
+            FPSNetworkPlayer targetPlayer = target.gameObject.GetComponent<FPSNetworkPlayer>();
             playerDamage.dealDamage(20);
+            Debug.Log(targetPlayer);
+            Debug.Log(playerHealth);
+            targetPlayer.configureHP();
+            targetPlayer.setHP(playerHealth.getHealth());
+            Debug.Log("player has " + playerHealth.getHealth() + " health left");
         } else {
             BotDamageable playerDamage = target.gameObject.GetComponent<BotDamageable>();
             playerDamage.dealDamage(20);
