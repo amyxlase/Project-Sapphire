2de2e970868b2172d33455e0cc13aa0bc47c3d84
popokun <gckim@andrew.cmu.edu>
2021-11-16 22:19:37 -0500

    implemented basic bot ai

--- Assets/Scripts/FPSNetworkBot.cs
+++ Assets/Scripts/FPSNetworkBot.cs
@@ -7,10 +7,26 @@ public class FPSNetworkBot : NetworkBehaviour
 {
     private CharacterController m_BotController;
     private Vector3 direction;
+    public EnemyState state;
+    public Transform shootOrigin;
+    public float detectionRange = 30f;
+    private bool isPatrolRoutineRunning;
+    public float patrolDuration = 3f;
+    public float idleDuration = 1f;
+    public float yVelocity = 0;
+    public float patrolSpeed = 2f;
+    public float shootRange = 15f;
+    public float chaseSpeed = 8f;
+    public FPSNetworkPlayer target;
+    public float gravity = -9.81f;
 
     void Start() {
+        state = EnemyState.patrol;
         m_BotController = GetComponent<CharacterController>();
-        direction = Vector3.zero;
+        //direction = Vector3.zero;
+        gravity *= Time.fixedDeltaTime * Time.fixedDeltaTime;
+        patrolSpeed *= Time.fixedDeltaTime;
+        chaseSpeed *= Time.fixedDeltaTime;
     }
 
     private void FixedUpdate() {
@@ -19,7 +35,7 @@ public class FPSNetworkBot : NetworkBehaviour
         AI();
 
         //Temporary for beta
-        BadAI();
+        //BadAI();
     }
 
     private void ApplyGravity() {
@@ -45,9 +61,157 @@ public class FPSNetworkBot : NetworkBehaviour
     //AI code should go here, these steps are a rough guideline but totally optional
     private void AI() {
         //1. Collect information on where other players are, health, etc
-
+        
         //2. Logic to decide action (walk in direction of players, avoid obstacles, shoot, etc)
-
+        switch(state)
+        {
+            case EnemyState.idle:
+                LookForPlayer();
+                break;
+            case EnemyState.patrol:
+                if(!LookForPlayer())
+                {
+                    Patrol();
+                }
+                break;
+            case EnemyState.chase:
+                Chase();
+                break;
+            case EnemyState.attack:
+                Attack();
+                break;
+            default:
+                break;
+        }
         //3. Execute chosen action
     }
+
+    public bool LookForPlayer() {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player"); 
+        foreach(GameObject player in players) {
+            Vector3 enemyToPlayer = player.transform.position - transform.position;
+            if(enemyToPlayer.magnitude <= detectionRange) {
+                if(Physics.Raycast(shootOrigin.position, enemyToPlayer, out RaycastHit hit, detectionRange)) {
+                    if (hit.collider.CompareTag("Player")) {
+                        target = hit.collider.GetComponent<FPSNetworkPlayer>();
+                        if (isPatrolRoutineRunning) {
+                            isPatrolRoutineRunning = false;
+                            StopCoroutine(StartPatrol());
+                        }
+                        state = EnemyState.chase;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void Patrol() {
+        if(!isPatrolRoutineRunning) {
+            StartCoroutine(StartPatrol());
+        }
+        Move(transform.forward, patrolSpeed);
+    }
+
+    private IEnumerator StartPatrol() {
+        isPatrolRoutineRunning = true;
+        //float x = Random.Range(-1f, 1f);
+        //float z = Random.Range(-1f, 1f);
+        Vector2 randomPatrolDirection = Random.insideUnitCircle.normalized;
+        transform.forward = new Vector3(randomPatrolDirection.x, 0f, randomPatrolDirection.y);
+       
+        yield return new WaitForSeconds(patrolDuration);
+        state = EnemyState.idle;
+        
+        yield return new WaitForSeconds(patrolDuration);
+        state = EnemyState.patrol;
+        isPatrolRoutineRunning = false;
+    }
+
+    private void Move(Vector3 direct, float speed) {
+        direct.y = 0f;
+        transform.forward = direct;
+        Vector3 movement = transform.forward * speed;
+        if(m_BotController.isGrounded) {
+            yVelocity = 0f;
+        }
+        yVelocity += gravity;
+        movement.y = yVelocity;
+        m_BotController.Move(movement);
+    }
+
+    private void Chase() {
+        if(CanSeeTarget()) {
+            Vector3 enemyToPlayer = target.transform.position - transform.position;
+
+            if(enemyToPlayer.magnitude <= shootRange) {
+                state = EnemyState.attack;
+            }
+            else {
+                Move(enemyToPlayer, chaseSpeed);
+            }
+        }
+        else {
+            target = null;
+            state = EnemyState.patrol;
+        }
+    }
+
+    private void Attack() {
+        if(CanSeeTarget()) {
+            Vector3 enemyToPlayer = target.transform.position - transform.position;
+            transform.forward = new Vector3(enemyToPlayer.x, 0f, enemyToPlayer.z);
+
+            if(enemyToPlayer.magnitude <= shootRange) {
+                Shoot();
+            }
+            else {
+                Move(enemyToPlayer, chaseSpeed);
+            }
+        }
+        else {
+            target = null;
+            state = EnemyState.patrol;
+        }
+    }
+
+    public void Shoot() {
+        RaycastHit hit;
+        Ray fromCamera =  Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(fromCamera, out hit, Mathf.Infinity)) {
+            if (hit.transform.gameObject.name == "FPSNetworkPlayerController(Clone)") {
+                CmdDealDamage(hit.transform);
+            }
+        }
+    }
+
+    [Command]
+    public void CmdDealDamage(Transform target) {
+        NetworkIdentity targetIdentity = target.gameObject.GetComponent<NetworkIdentity>();
+        Damageable playerDamage = target.gameObject.GetComponent<Damageable>();
+        Health playerHealth = target.gameObject.GetComponent<Health>();
+        Shield playerShield = target.gameObject.GetComponent<Shield>();
+        playerDamage.dealDamage(20);
+    }
+
+    private bool CanSeeTarget() {
+        if(target == null) {
+            return false;
+        }
+        if(Physics.Raycast(shootOrigin.position, target.transform.position - transform.position, out RaycastHit hit , detectionRange)) {
+            if(hit.collider.CompareTag("Player")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public enum EnemyState {
+        idle,
+        patrol,
+        chase,
+        attack
+    }
+
 }
