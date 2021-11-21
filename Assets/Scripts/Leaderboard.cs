3922f0672fe091898dfa0b3e585b253d8d3cfdd8
monal20 <monalin2002@andrew.cmu.edu>
2021-11-21 17:23:48 -0500

    Buggy Leaderboard
    Need to properly set up top scores sorting/parsing

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 using TMPro;
+using static System.Math; 
+
+public class ITEM
+{
+    public string itemID {get; set;}
+    public string itemKills {get; set;}
+}
 
 public class Leaderboard : MonoBehaviour
 {    
@@ -12,43 +19,57 @@ public class Leaderboard : MonoBehaviour
     public Text Test;
     public GameObject rowPrefab;
     public Transform rowsParent;
+    bool setUp = false;
+    int countPlayers = 0;
 
     void OnEnable (){
-    
-        //Get array of all players
-        //NetworkIdentity [] players = NetworkServer.spawned.Values.ToArray();
-        //Set up list elements for each player
+
+        //Setup row game objects
+        if (!setUp){
+                for (int i = 0; i < 8; i++){
+                    GameObject newRow = Instantiate(rowPrefab, rowsParent);
+                } 
+                setUp = true;
+            }
     
 
+        //Loop Through Player Data
         var players = FindObjectsOfType<FPSNetworkPlayer>();
+        // public PriorityQueue<uint, int> topKills = new PriorityQueue<uint, int>(new KillComparer());
+        public List<ITEM> topKills = new List<ITEM>();
         foreach (FPSNetworkPlayer player in players){
             uint ID = player.GetComponent<NetworkIdentity>().netId;
-            //TextMeshPro [] texts = newRow.GetComponentsInChildren<TextMeshPro>();
-            //get tags find object of rag
-            //texts[0].text = ID.ToString();
-            //texts[1].text = player.kills.ToString();
-            GameObject [] texts = GameObject.FindGameObjectsWithTag("rowComponent");
-            GameObject newRow = Instantiate(rowPrefab, rowsParent);
-            for (int i = 0; i < texts.Length; i++){
-                int s = i / 4;
-                if (i % 4 == 0){
-                    texts[i].transform.localPosition = new Vector3(-140, 60 + s*(-40), 0);
-                    texts[i].GetComponent<TMPro.TextMeshProUGUI>().text = ID.ToString();
-                }
-                else if (i % 4 == 1){
-                    texts[i].transform.localPosition = new Vector3(70, 60 + s*(-40), 0);
-                    texts[i].GetComponent<TMPro.TextMeshProUGUI>().text = player.kills.ToString();
-                }
-                else if (i % 4 == 2)
-                    texts[i].transform.localPosition = new Vector3(140, 60 + s*(-40), 0);
-                else if (i % 4 == 3)
-                    texts[i].transform.localPosition = new Vector3(10, 60 + s*(-40), 0);
+            topKills.Add(new ITEM {itemID = ID, itemKills = player.kills});
+            countPlayers += 1;
+            Debug.Log("Num Kills: " + player.kills + " ID: " + ID);
             }
 
+        //Sort Player Data
+        topKills = topKills.OrderBy(w => w.itemKills).ToList();
 
-    
-
-            Debug.Log("Num Kills: " + player.kills + " ID: " + ID);
+        GameObject [] texts = GameObject.FindGameObjectsWithTag("rowComponent");
+        // Draw 8 pink rows
+        for (int i = 0; i < texts.Length; i++){
+                int s = i / 4;
+                if (i % 4 == 3)
+                    texts[i].transform.localPosition = new Vector3(10, 60 + s*(-40), 0);
+            }
+        // Display Top players
+        for (int i = 0; i < Min(countPlayers*4, 32); i++){
+            int s = i / 4;
+            //name
+            if (i % 4 == 0){
+                texts[i].transform.localPosition = new Vector3(-140, 60 + s*(-40), 0);
+                texts[i].GetComponent<TMPro.TextMeshProUGUI>().text = topKills[s].itemID.ToString();
+            }
+            //num kills
+            else if (i % 4 == 1){
+                texts[i].transform.localPosition = new Vector3(70, 60 + s*(-40), 0);
+                texts[i].GetComponent<TMPro.TextMeshProUGUI>().text = topKills[s].itemKills.ToString();
+            }
+            //Time in Game
+            else if (i % 4 == 2)
+                texts[i].transform.localPosition = new Vector3(140, 60 + s*(-40), 0);
         }
     }
 
